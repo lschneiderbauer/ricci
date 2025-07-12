8ae716084fc008bc738ea3151d761c61a9194d58
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2025-07-12 21:04:56 +0200

    more docs / metric tensor refinements

--- R/tensor_instances.R
+++ R/tensor_instances.R
@@ -4,15 +4,19 @@
 #' case of two labels this represents simply the identity matrix.
 #'
 #' @param n The dimension.
-#' @return A function that expects index labels. See [.()].
+#' @return
+#'  A function that expects index labels (see [.()]) and returns a
+#'  labeled tensor. The underlying data will differs depending on
+#'  the number of labels provided.
 #'
-#' @seealso [calculus::delta()]
+#' @seealso Underlying implementation: [calculus::delta()]
 #' @examples
 #' d(3)(i, j)
 #'
 #' d(3)(i, j, k, l)
 #' @export
 #' @concept tensor_instance
+#' @family tensor symbols
 d <- function(n) {
   function(...) {
     i <- .(...)
@@ -33,14 +37,19 @@ d <- function(n) {
 #' Provides a labeled Levi Civita epsilon tensor.
 #'
 #' @inheritParams .
+#' @return
+#'  A labeled tensor object.
+#'  The underlying data will differs depending on
+#'  the number of labels provided.
 #'
-#' @seealso [calculus::epsilon()]
+#' @seealso Underlying implementation: [calculus::epsilon()]
 #' @examples
 #' e(i, j)
 #'
 #' e(i, j, k)
 #' @export
 #' @concept tensor_instance
+#' @family tensor symbols
 e <- function(...) {
   i <- .(...)
 
@@ -54,9 +63,15 @@ e <- function(...) {
 #' Minkowski metric tensor
 #'
 #' Provides a labeled Minkowski metric tensor in `n` dimensions.
+#' Note that the numeric result will be different depending on the
+#' chosen index positions.
+#' If the labels are left empty, and unlabeled n by n
+#' array of the covariant metric tensor is returned.
 #'
 #' @param n The dimension.
-#' @return A function that expects index labels. See [.()].
+#' @return
+#'  A function that expects index labels (see [.()]) and returns a
+#'  labeled tensor.
 #'
 #' @examples
 #' g_mink(4)(i, j)
@@ -64,14 +79,20 @@ e <- function(...) {
 #' g_mink(4)(+i, +j)
 #' @export
 #' @concept tensor_instance
+#' @family metric tensors
 g_mink <- function(n) {
   function(...) {
     i <- .(...)
+
+    if (length(i$i) == 0) {
+      return(diag(c(-1, rep(1, n - 1)), n, n))
+    }
 
     if (setequal(i$p, "+") || setequal(i$p, "-")) {
       # in this case the inverse is equal to g,
       # so we don't need to calculate it
       g <- diag(c(-1, rep(1, n - 1)), n, n)
+
       tensor(g, i$i, i$p)
     } else {
       d(n)(...)
@@ -79,12 +100,19 @@ g_mink <- function(n) {
   }
 }
 
+
 #' Euclidean metric tensor
 #'
 #' Provides a labeled Euclidean metric tensor in `n` dimensions.
+#' Note that the numeric result will be different depending on the
+#' chosen index positions.
+#' If the labels are left empty, an unlabeled n by n
+#' array of the covariant metric tensor is returned.
 #'
 #' @param n The dimension.
-#' @return A function that expects index labels. See [.()].
+#' @return
+#'  A function that expects index labels (see [.()]) and returns a
+#'  labeled tensor.
 #'
 #' @examples
 #' g_eucl(4)(i, j)
@@ -92,10 +120,15 @@ g_mink <- function(n) {
 #' g_eucl(4)(+i, +j)
 #' @export
 #' @concept tensor_instance
+#' @family metric tensors
 g_eucl <- function(n) {
   function(...) {
     i <- .(...)
 
-    tensor(diag(1, n, n), i$i, i$p)
+    if (length(i$i) == 0) {
+      diag(1, n, n)
+    } else {
+      tensor(diag(1, n, n), i$i, i$p)
+    }
   }
 }
